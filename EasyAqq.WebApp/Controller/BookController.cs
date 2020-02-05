e476b0469bf8eb876cea69807268e5c090c68ad9
Cong Le <conghoang1988@gmail.com>
2020-02-05 22:10:44 +0700

    Initialize project

---
+++ EasyAqq.WebApp/Controller/BookController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using EasyAqq.WebApp.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyAqq.WebApp.Controller
+{
+    [ApiController]
+    public class BookController : Microsoft.AspNetCore.Mvc.Controller
+    {
+
+        public readonly ApplicationDbContext _db;
+
+        
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("api/book")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new {data = _db.Books.ToList()});
+        }
+    }
+}
---
+++ EasyAqq.WebApp/Migrations/20200202071703_UpdateAuthorToString.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyAqq.WebApp.Migrations
+{
+    public partial class UpdateAuthorToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Author",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
